4a2ca089f055f964da665978a9f67044751bd91f
vjz3qz <vjz3qz@virginia.edu>
2023-03-08 15:55:34 -0500

    added express server

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.send("hello");
+});
+
+app.listen(3000, function() {
+    console.log("Server running on port 3000");
+});
