03639d80669ed3d22e938382433fa9565dce9061
vjz3qz <vjz3qz@virginia.edu>
2023-03-08 16:07:02 -0500

    added sign up html and css

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-    res.send("hello");
+    res.sendFile(__dirname+"/signup.html");
 });
 
 app.listen(3000, function() {
